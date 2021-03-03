12b5d9442eda592174d992a75c734bf9349dd96e
Franco M. Thomas <74632584+francnitty@users.noreply.github.com>
2021-03-03 08:02:50 -0500

    Still need to figure out why code isn't displaying desired content on site, then work on script

--- web/amazon.js
+++ web/amazon.js
@@ -16,10 +16,35 @@
 //     container.appendChild(div);
 // }
 
-const myHeading = document.getElementById('myheading')
-document.getElementById("myheading").addEventListener("mouseover", function() {
-    document.getElementById("myheading").style.color = "green";
-});
-document.getElementById("myheading").addEventListener("mouseout", function() {
-    document.getElementById("myheading").style.color = "red";
-});
+const container = document.querySelector(".grid-container");
+
+function showItems(){
+  
+    let card = document.createElement('div');
+    card.className = "card";
+    let cardInnerHtml = 
+    `
+                <div class="container">
+                    <img src="" alt="t-shirt">
+                </div>
+                <div class="info">
+                    <p>Product 1</p>
+                    <p class="price">$100.00</p>
+                    <button>Add to cart</button>
+                </div>
+    `
+    card.innerHTML = cardInnerHtml;
+
+    return card;
+}
+
+container.appendChild(showItems());
+
+
+// const myHeading = document.getElementById('myheading')
+// document.getElementById("myheading").addEventListener("mouseover", function() {
+//     document.getElementById("myheading").style.color = "green";
+// });
+// document.getElementById("myheading").addEventListener("mouseout", function() {
+//     document.getElementById("myheading").style.color = "red";
+// });
